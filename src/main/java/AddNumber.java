f9433e3e76449c56677b4efe0fc74bd8a8375688
Lavina <corporate19.lavina@gmail.com>
2021-12-02 14:31:01 +0530

    new add

---
+++ src/main/java/AddNumber.java
@@ -0,0 +1,13 @@
+
+public class AddNumber {
+	public int addNumber(int a, int b) {
+		int c=20;
+		System.out.println("Hello lavina");;
+		return a+b;
+	}
+	public int SUBNumber(int a, int b) {
+		return a-b;
+	}
+	
+
+}
